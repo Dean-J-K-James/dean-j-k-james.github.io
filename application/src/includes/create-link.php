f88010e69d7849ad41715cb5ff02c6aa796c78a4
Dean James <dean.j.k.james@gmail.com>
2022-05-23 22:04:39 +0100

    New CSS class for menu and cook

--- application/src/includes/create-link.php
+++ application/src/includes/create-link.php
@@ -18,9 +18,9 @@ function load_file($file) { return $_SERVER['DOCUMENT_ROOT'] . load_asset($file)
 /**
  * 
  */
-function load_asset($file) { return brand_asset_exists($file) ? APPPATH . "/application/" . BRAND . "/" . $file : APPPATH . "/src/" . $file; }
+function load_asset($file) { return asset_exists($file) ? APPPATH . "/application/" . BRAND . "/" . $file : APPPATH . "/src/" . $file; }
 
 /**
  * 
  */
-function brand_asset_exists($file) { return file_exists($_SERVER['DOCUMENT_ROOT'] . APPPATH . "/application/" . BRAND . "/" . $file); }
+function asset_exists($file) { return file_exists($_SERVER['DOCUMENT_ROOT'] . APPPATH . "/application/" . BRAND . "/" . $file); }
--- application/src/includes/menu.php
+++ application/src/includes/menu.php
@@ -1,4 +1,4 @@
-<div id="menu" class="flex-row flex-08">
+<div id="menu" class="toolbar flex-row flex-08">
     <a href="<?= create_link(HOMEPGE) ?>" class="logo"><?= $_SESSION['brand']['logo'] ?></a>
     <div class="flex-fill"></div>
     <?php require_once 'pages/menu/brand.php' ?>
